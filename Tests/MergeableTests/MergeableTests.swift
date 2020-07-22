81ea94d1a6a04f366608b2416a727c5f11d20c28
Guerson <ultim8p@gmail.com>
2020-07-21 22:50:48 -0500

    Decreased minimum iOS version to v9.

--- Tests/MergeableTests/MergeableTests.swift
+++ Tests/MergeableTests/MergeableTests.swift
@@ -7,7 +7,7 @@ final class MergeableTests: XCTestCase {
     func testMerge(){
         var start = TestModel(_id: "01", name: "Start", code: 20)
         let startChanged =  TestModel(_id: "01", name: nil, code: 40)
-        _ = start.merge(TestModel.self, with: startChanged)
+        _ = start.merge(with: startChanged)
         XCTAssertEqual(start.code, 40)
         XCTAssertEqual(start.name, "Start")
 
